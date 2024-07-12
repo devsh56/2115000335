53626381464a21fab893c69439666d2f2555af6e
devsh56 <100186354+devsh56@users.noreply.github.com>
2024-07-12 15:50:47 +0530

    Question 1

--- RentFashion-main/RentFashion-main/routes/session.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const session = require('express-session')
-const router=express.Router();
-router.get('/session',(req,res)=>{
-    res.send("welcome to session ")
-})
-
-
-module.exports=router;
